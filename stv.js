d370eb11a3c4c577d505a5cf517ceaa2ecdacd91
Gavin Goff <gavinpatrickgoff@gmail.com>
2024-07-22 11:44:50 -0500

    hi

---
+++ stv.js
@@ -0,0 +1,19 @@
+/*
+ Set the constant variable, 'str', to be the openning message.
+ When the alert is put in, I can put the variable, 'str', at the beginning.
+ */
+const str = 'You have received this message because you have been chosen to open an important vault. Here is the secret combination:'
+
+/*
+This are the arithmetic equations for the vault code. 
+The code is 10 - 40 - 39, so each equation is equal to those numbers in sequence.
+*/
+let num1 = 14 - 4
+let num2 = 8 * 5
+let num3 = 19 + 20
+
+/*
+This line is for the final popup message.
+Including all the variables in sequence like this will cause the appropriate message to appear.
+*/
+alert(`{str} {num1} - {num2} - {num3}`)
